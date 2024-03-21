343423014fdb095b99969de98159865a2eb0e0a3
Joe Kadlic <joe.kadlic@impower.ai>
2024-03-21 12:45:03 -0400

    Added local directory structure validation

--- lib/config/env.config.ts
+++ lib/config/env.config.ts
@@ -3,15 +3,43 @@ import dotenv from 'dotenv-safe';
 dotenv.config({ allowEmptyValues: true });
 
 import path from 'path';
+import fs from 'fs';
+import { createDirIfNotExists } from '../utils/file.utils';
 
-// Define directroy locations
+/*
+    Desired local directory structure
+
+    root/
+    - spec/
+    - debug/
+      - data/
+        - instance/
+        - schema/
+      - contexts/
+      - reports/
+*/
+
+// Define and validate test directory
 export const TEST_DIR = path.join(process.cwd(), 'spec');
+if (!fs.existsSync(TEST_DIR))
+    throw new Error(`Test directory does not exist: ${TEST_DIR}`);
+
+// Define and validate debug directory
 export const DEBUG_DIR = path.join(process.cwd(), 'debug');
-export const STATE_DIR = path.join(DEBUG_DIR, 'state');
-export const REPORT_DIR = path.join(DEBUG_DIR, 'reports');
+createDirIfNotExists(DEBUG_DIR);
+export const CONTEXT_DIR = path.join(DEBUG_DIR, 'context');
+createDirIfNotExists(CONTEXT_DIR);
+export const REPORTS_DIR = path.join(DEBUG_DIR, 'reports');
+createDirIfNotExists(REPORTS_DIR);
+export const DATA_DIR = path.join(DEBUG_DIR, 'data');
+createDirIfNotExists(DATA_DIR);
+export const DATA_SCHEMA_DIR = path.join(DATA_DIR, 'schema');
+createDirIfNotExists(DATA_SCHEMA_DIR);
+export const DATA_INSTANCE_DIR = path.join(DATA_DIR, 'instance');
+createDirIfNotExists(DATA_INSTANCE_DIR);
 
 // Define default file paths
-export const DEFAULT_STORAGE_STATE = path.join(STATE_DIR, 'default_storage_state.json');
+export const DEFAULT_STORAGE_STATE = path.join(CONTEXT_DIR, 'default_browser_context.json');
 
 // Define report output paths
-export const MONOCART_REPORT = path.join(REPORT_DIR, 'monocart/report.html');
+export const MONOCART_REPORT = path.join(REPORTS_DIR, 'monocart/report.html');
--- lib/utils/file.utils.ts
+++ lib/utils/file.utils.ts
@@ -1,8 +1,12 @@
-import { existsSync, statSync } from 'fs';
+import fs from 'fs';
 
 export function getFileAgeInMinutes(path: string): number {
-    if (!existsSync(path)) return -1;
-    const ageMs: number = statSync(path).mtimeMs;
+    if (!fs.existsSync(path)) return -1;
+    const ageMs: number = fs.statSync(path).mtimeMs;
     const diffInMinutes: number = Math.floor((Date.now() - ageMs) / (1000 * 60));
     return diffInMinutes;
+}
+
+export function createDirIfNotExists(path: string) {
+    if (!fs.existsSync(path)) fs.mkdirSync(path, { recursive: true });
 }

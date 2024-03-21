8fbf851dd40269530bbafeff08cea434113e4b7c
Joe Kadlic <joe.kadlic@impower.ai>
2024-03-21 10:48:11 -0400

    Cleaned up playwright.config.ts

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,91 +6,40 @@ dotenv.config({
 });
 dotenv.config();
 
-
-
-
-/**
- * Read environment variables from file.
- * https://github.com/motdotla/dotenv
- */
-// require('dotenv').config();
-
-/**
- * See https://playwright.dev/docs/test-configuration.
- */
 export default defineConfig({
-  testDir: './spec',
-  /* Run tests in files in parallel */
-  fullyParallel: true,
-  /* Fail the build on CI if you accidentally left test.only in the source code. */
-  forbidOnly: !!process.env.CI,
-  /* Retry on CI only */
-  retries: process.env.CI ? 2 : 0,
-  /* Opt out of parallel tests on CI. */
-  workers: process.env.CI ? 1 : undefined,
-  /* Reporter to use. See https://playwright.dev/docs/test-reporters */
-  //reporter: 'html',
-  reporter: [
-    ['monocart-reporter', {
-      name: "Monocart Report",
-      outputFile: `debug/test-results/monocart/report.html`
-    }],
-  ],
-  /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
-  use: {
-    /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'https://agilitypartners-dev-ed.develop.lightning.force.com/lightning',
-    storageState: `${process.cwd()}/debug/browser/default_browser_state.json`,
-    /* Collect trace when retrying the failed test. See https://playwright.dev/docs/trace-viewer */
-    trace: 'on-first-retry'
-  },
-  
-  globalSetup: `${process.cwd()}/lib/hooks/globalSetup.ts`,
-  globalTeardown: `${process.cwd()}/lib/hooks/globalTeardown.ts`,
-  
-  /* Configure projects for major browsers */
-  projects: [
-    {
-      name: 'chromium',
-      use: { ...devices['Desktop Chrome'] },
-    }
-    //,
 
-    // {
-    //   name: 'firefox',
-    //   use: { ...devices['Desktop Firefox'] },
-    // },
+	/* Configure basic project settings */
+	testDir: './spec',
+	fullyParallel: true,
+	forbidOnly: !!process.env.CI, // Fail the build on CI if you accidentally left test.only in the source code.
+	retries: process.env.CI ? 2 : 0, // Retry on CI only
+	workers: process.env.CI ? 1 : undefined, // Opt out of parallel tests on CI.
 
-    // {
-    //   name: 'webkit',
-    //   use: { ...devices['Desktop Safari'] },
-    // },
+	/* Configure reporters */
+	// TODO : Different reporters for CI ?
+	reporter: [
+		['monocart-reporter', {
+			name: "Monocart Report",
+			outputFile: `debug/test-results/monocart/report.html`
+		}],
+	],
 
-    /* Test against mobile viewports. */
-    // {
-    //   name: 'Mobile Chrome',
-    //   use: { ...devices['Pixel 5'] },
-    // },
-    // {
-    //   name: 'Mobile Safari',
-    //   use: { ...devices['iPhone 12'] },
-    // },
+	/* Configure setup & teardown for all projects */
+	globalSetup: `${process.cwd()}/lib/hooks/globalSetup.ts`,
+	globalTeardown: `${process.cwd()}/lib/hooks/globalTeardown.ts`,
 
-    /* Test against branded browsers. */
-    // {
-    //   name: 'Microsoft Edge',
-    //   use: { ...devices['Desktop Edge'], channel: 'msedge' },
-    // },
-    // {
-    //   name: 'Google Chrome',
-    //   use: { ...devices['Desktop Chrome'], channel: 'chrome' },
-    // },
-  ],
+	/* Configure global options for all tests  */
+	use: {
+		baseURL: 'https://agilitypartners-dev-ed.develop.lightning.force.com/lightning',
+		storageState: `${process.cwd()}/debug/browser/default_browser_state.json`,
+		trace: 'on-first-retry',
+	},
 
-  /* Run your local dev server before starting the tests */
-  // webServer: {
-  //   command: 'npm run start',
-  //   url: 'http://127.0.0.1:3000',
-  //   reuseExistingServer: !process.env.CI,
-  // },
+	/* Configure all projects */
+	projects: [
+		{
+			name: 'chromium',
+			use: { ...devices['Desktop Chrome'] },
+		}
+	],
 });
